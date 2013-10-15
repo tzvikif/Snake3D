038b0791a3e1ffc48fb9cdacd346162db2dcc62d
tzvikif <tzvikif@gmail.com>
2013-10-16 00:10:00 +0300

    moving constans to global scope

--- Snake3D/Consts.h
+++ Snake3D/Consts.h
@@ -7,9 +7,10 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import "Vectors.h"
 extern int const N;
 extern float const totalOrientationTime;
+extern float const SNKspeed;
 typedef enum _PROGRAMS {
     PROG_FLOOR,
     PROG_SNAKE
@@ -38,4 +39,30 @@ typedef enum _ORIENTATION {
 }ORIENTATION;
 
 float lerp(float a,float b,float blend);
+
+//UL
+extern CC3Vector const ul_lookAt;
+extern CC3Vector const ul_eyeAt;
+extern CC3Vector const ul_up;
+//UR
+extern CC3Vector const ur_lookAt;
+extern CC3Vector const ur_eyeAt;
+extern CC3Vector const ur_up;
+//BL
+extern CC3Vector const bl_lookAt;
+extern CC3Vector const bl_eyeAt;
+extern CC3Vector const bl_up;
+//BR
+extern CC3Vector const br_lookAt;
+extern CC3Vector const br_eyeAt;
+extern CC3Vector const br_up;
+
+extern GLfloat const cube_verticesX[];
+extern GLfloat const cube_vertices[];
+extern GLfloat const cube_colorsX[];
+extern GLushort const cube_elementsX[];
+extern GLushort const cube_elements[];
+extern GLfloat const cube_colors[];
+extern GLfloat const cube_normals[];
+
 
